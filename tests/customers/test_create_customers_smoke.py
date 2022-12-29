c83aef579586d096d5756fa3aa59070fc63a7beb
jomina-jolly <jominajolly@gmail.com>
2022-12-28 23:23:06 -0800

    added API response assertion on email id

--- tests/customers/test_create_customers_smoke.py
+++ tests/customers/test_create_customers_smoke.py
@@ -1,6 +1,7 @@
 import pytest
 import logging as logger
 from src.utilites.genericUtilities import generate_random_email_and_password
+from src.helpers.customers_helper import CustomerHelper
 
 pytest.mark.tcid29
 def test_create_customer_email_passowrd_only():
@@ -10,12 +11,20 @@ def test_create_customer_email_passowrd_only():
     rand_info = generate_random_email_and_password()
     email = rand_info['email']
     password  = rand_info['password']
-
-    logger.info("Email and password: {},{}".format(email,password))
+    logger.info("Email and password: {},{}".format(email, password))
+    
     #create payload
+    payload = {'email': email, 'password':password}
 
     #make the call
+    #Creates the payload
+    customer_obj = CustomerHelper()
+    customer_api_info = customer_obj.create_customer(email=email, password=password)
 
+    #verify email id in response
+    assert customer_api_info['email'] == email, "Customer API returned wrong email id"
+    import pdb; pdb.set_trace()
+    
     #verify status call
 
     #verify email id in response
