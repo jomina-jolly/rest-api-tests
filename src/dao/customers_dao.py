5760e2d63531acf07e00c507e48d124027479bdd
jomina-jolly <jominajolly@gmail.com>
2022-12-29 22:50:40 -0800

    DB connection, customer dao

--- src/dao/customers_dao.py
+++ src/dao/customers_dao.py
@@ -1,7 +1,13 @@
-
+from src.utilites.dbUtilities import DBUtilities
 
 class CustomerDAO():
 
+    def __init__(self) -> None:
+        self.db_helper = DBUtilities()
+
+
     def get_customer_by_email(self, email):
 
-        query = f"SELECT * FROM marketplace.wp_users WHERE user_email = '{email}';"
+        sql = f"SELECT * FROM marketplace.wp_users WHERE user_email = '{email}';"
+        resp_dic = self.db_helper.execute_select(sql=sql)
+        return resp_dic
