b865f9863c663584e55221e3656e46e6124c4708
jomina-jolly <jominajolly@gmail.com>
2023-01-04 22:51:43 -0800

    create customers with existing user's email id

--- src/dao/customers_dao.py
+++ src/dao/customers_dao.py
@@ -1,13 +1,17 @@
 from src.utilites.dbUtilities import DBUtilities
+import random
 
 class CustomerDAO():
 
     def __init__(self) -> None:
         self.db_helper = DBUtilities()
 
-
     def get_customer_by_email(self, email):
-
         sql = f"SELECT * FROM marketplace.wp_users WHERE user_email = '{email}';"
         resp_dic = self.db_helper.execute_select(sql=sql)
         return resp_dic
+
+    def get_random_customer(self, qty=1):
+        sql = f"SELECT * FROM marketplace.wp_users ORDER BY ID DESC LIMIT 500;"
+        customer_list_db_rsp = self.db_helper.execute_select(sql=sql)
+        return random.sample(customer_list_db_rsp, qty)
--- src/utilites/requestUtilities.py
+++ src/utilites/requestUtilities.py
@@ -15,10 +15,10 @@ class RequestsUtilites():
         api_credentials = CredentialUtilites.get_wc_api_keys()
         API_KEY = api_credentials['wc_key']
         API_SECRET = api_credentials['wc_secret']
-        self.auth = OAuth1("API_KEY", "API_SECRET")
+        self.auth = OAuth1(API_KEY, API_SECRET)
 
     def assert_status_code(self):
-        assert self.expected_status_code == self.assert_status_code, \
+        assert self.expected_status_code == self.status_code, \
             f"Expected status code {self.expected_status_code} but got {self.status_code} \n\
             for the URL {self.url} \n \
             respose json: {self.rs_json}"
@@ -37,8 +37,24 @@ class RequestsUtilites():
         #Check the status code
         self.assert_status_code()
 
-        logger.debug[f'Customer API response {self.rs_json}']
+        logger.debug(f'POST API response {self.rs_json}')
         return self.rs_json
         
-    def get(self):
-        pass
+    def get(self, endpoint, params=None, headers=None, expected_status_code=200):
+        
+        self.expected_status_code = expected_status_code
+
+        if not headers:
+            headers = {"Content-Type": "application/json"}
+        
+        self.url = self.base_url + endpoint
+
+        rs_api = requests.get(url=self.url, data=json.dumps(params), headers=headers, auth=self.auth)
+        self.status_code = rs_api.status_code
+
+        self.rs_json = rs_api.json()
+        #Check the status code
+        self.assert_status_code()
+        logger.debug(f'GET API response {self.rs_json}')
+        
+        return self.rs_json
